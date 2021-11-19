353d4e66ec996a5bf01c3e50d8fc429c3aa21246
mohammad mehdi vahedi <65023547+mahdivahedi@users.noreply.github.com>
2021-11-19 22:45:17 +0330

    Add files via upload
    webserver by nodemcu and slide range. 
    If the slider range value reduces to 300, The led and buzzer will turn on otherwise they will remain off.

---
+++ ESP8266/Machine_Sensor/Machine_Sensor.ino
@@ -0,0 +1,119 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncTCP.h>
+
+// Mohammad mahdi vahedi      9731069  
+
+// Replace with your network credentials
+const char* ssid = "POCO X3 NFC";
+const char* password = "12345678";
+
+const int output = 2;
+const int output_buzzer = 16;
+
+String sliderValue = "0";
+
+const char* PARAM_INPUT = "value";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Question 8 Web Server</title>
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.1rem;}
+    p {font-size: 2.1rem;}
+    body {max-width: 400px; margin:0px auto; padding-bottom: 25px;}
+    .slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: #FFD65C;
+      outline: none; -webkit-transition: .2s; transition: opacity .2s;}
+    .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: #003249; cursor: pointer;}
+    .slider::-moz-range-thumb { width: 35px; height: 35px; background: #003249; cursor: pointer; } 
+  </style>
+</head>
+<body>
+  <h2>Question 8 Web Server</h2>
+  <p><span id="textSliderValue">%SLIDERVALUE%</span></p>
+  <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="0" max="900" value="%SLIDERVALUE%" step="1" class="slider"></p>
+<script>
+function updateSliderPWM(element) {
+  var sliderValue = document.getElementById("pwmSlider").value;
+  document.getElementById("textSliderValue").innerHTML = sliderValue;
+  console.log(sliderValue);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/slider?value="+sliderValue, true);
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if (var == "SLIDERVALUE"){
+    return sliderValue;
+  }
+  return String();
+}
+
+void setup(){
+  pinMode(output, OUTPUT);
+  pinMode(output_buzzer, OUTPUT);
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  analogWrite(output, sliderValue.toInt());
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/slider?value=<inputMessage>
+  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
+    if (request->hasParam(PARAM_INPUT)) {
+      inputMessage = request->getParam(PARAM_INPUT)->value();
+      sliderValue = inputMessage;
+      analogWrite(output, sliderValue.toInt());
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+  
+  // Start server
+  server.begin();
+}
+  
+void loop() {
+
+  if (sliderValue.toInt() < 300 ){
+    digitalWrite(output, HIGH);
+    digitalWrite(output_buzzer, HIGH);
+    delay(1000);
+  } else {
+    digitalWrite(output, LOW);
+    digitalWrite(output_buzzer, LOW);
+    delay(1000);
+  }
+  
+}
